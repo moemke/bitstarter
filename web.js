c5120bf34112f5c10c60edaa0a47a601333de441
Gaya Moemke <gaya.moemke@gmail.com>
2013-07-10 18:54:55 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html'));
+    response.send(fs.readFileSync(infile));
     //response.send('Hello World2!');
 });
 
