4742fa6be39b19d4271a78da6e64775d578434ce
Gaya Moemke <gaya.moemke@gmail.com>
2013-07-18 12:08:54 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-var infile = "index.html";
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(fs.readFileSync(infile));
 
 app.get('/', function(request, response) {
-    response.send(buf.toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
     //response.send('Hello World2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
